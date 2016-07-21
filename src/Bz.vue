7223cb11c0a6318ec037b1f2e5ec9b4fe0f74b94
bigzhu <vermiliondun@gmail.com>
2016-07-21 23:10:10 +0800

    init

---
+++ src/Bz.vue
@@ -0,0 +1,73 @@
+<style lang="less">
+  #map_container {
+    width: 100%;
+    height: 100%;
+  }
+  /*屏蔽腾讯logo */
+  div[style="position: absolute; left: 0px; top: 0px;"], // 左上角按钮
+  div[style="position: absolute; z-index: 1000000; -webkit-user-select: none; left: 7px; bottom: 0px; "],
+  div[style="position: absolute; z-index: 1000000; margin: 2px 5px 0px 2px; left: 0px; bottom: 0px; "],
+  div[style="position: absolute; z-index: 1000000; -webkit-user-select: none; left: 7px; bottom: 0px;"],
+  div[style="position: absolute; z-index: 1000000; margin: 2px 5px 0px 2px; left: 0px; bottom: 0px;"],
+  div[style="position: absolute; z-index: 1000000; -webkit-user-select: none; left: 87px; bottom: 0px; "],
+  div[style="position: absolute; z-index: 1000000; -webkit-user-select: none; left: 87px; bottom: 0px;"] {
+    display: none;
+  }
+</style>
+
+<template>
+  <div>
+    <script-loader :scripts="scripts"></script-loader>
+    <div id="map_container"></div>
+  </div>
+</template>
+
+<script>
+  import ScriptLoader from 'bz-script-loader'
+  export default {
+    props: {
+      config_map: {
+        default: null
+      },
+      map: {
+        type: Object,
+        required: true,
+        twoWay: true
+      }
+    },
+    components: {
+      ScriptLoader
+    },
+    computed: {
+    },
+    data: function () {
+      return {
+        scripts: [
+          '//map.qq.com/api/js?v=2.exp&callback=initMap'
+        ]
+      }
+    },
+    ready () {
+      window.initMap = this.initMap
+    },
+    methods: {
+      initMap: function () {
+        this.map = new window.qq.maps.Map(
+          document.getElementById('map_container'), {
+            zoom: 12,
+            zoomControl: false,
+            // mapTypeId: window.qq.maps.MapTypeId.HYBRID, // ROADMAP, SATELLITE, HYBRID
+            draggable: true,
+            draggableCursor: 'crosshair',
+            scrollwheel: true,
+            disableDoubleClickZoom: true,
+            mapTypeControl: false
+          }
+        )
+        if (this.config_map) {
+          this.config_map(this.map)
+        }
+      }
+    }
+  }
+</script>
---
+++ src/Doc.vue
@@ -0,0 +1,75 @@
+<style lang="less">
+  .original-text-bz {
+    /*保留空格*/
+    white-space: pre-wrap;
+    /*字体能自动换行*/
+    word-wrap:break-word;
+  }
+  code {
+    padding: 2px 4px;
+    font-size: 90%;
+    color: #c7254e;
+    background-color: #f9f2f4;
+    border-radius: 4px;
+    .original-text-bz;
+  }
+  .map {
+    width: 100%;
+    height: 500px;
+  }
+</style>
+
+<template>
+  <div class="ui container segment">
+    <h1>{{name}}</h1>
+    <p>
+      {{desc}}
+    </p>
+    <table class="ui celled table">
+      <thead>
+        <tr><th>参数</th><th>说明</th></tr>
+      </thead>
+      <tbody>
+        <tr v-for="parm in parms"> <td class="single line"> {{parm.parm}} </td> <td> {{parm.desc}} </td></tr>
+        <tr>
+          <td colspan="2"></td>
+        </tr>
+      </tbody>
+    </table>
+    <code v-text="code">
+    </code>
+    <div class="ui divider"></div>
+    <q-map :map.sync="map" :config_map="config_map" class="map"></q-map>
+  </div>
+</template>
+
+<script>
+  import 'semantic-ui/dist/components/site.css'
+  import 'semantic-ui/dist/components/reset.css'
+  import 'semantic-ui/dist/components/table.css'
+  import 'semantic-ui/dist/components/container.css'
+  import 'semantic-ui/dist/components/divider.css'
+  import QMap from './Bz.vue'
+  export default {
+    components: {
+      QMap
+    },
+    data: function () {
+      return {
+        name: 'Qmap',
+        desc: '目前就是简单的显示了一个腾讯地图, 解决了资源加载问题, 但是污染了window.initMap 名字空间, 以后功能再添加',
+        parms: [
+          { parm: 'map', desc: '双向绑定的map实例' },
+          { parm: 'config_map', desc: '自定义的参数修改, map 做为参数传进来' }
+        ],
+        map: {},
+        code: `<q-map></q-map>`
+      }
+    },
+    methods: {
+      config_map: function (map) {
+        console.log(map)
+      }
+    }
+  }
+</script>
